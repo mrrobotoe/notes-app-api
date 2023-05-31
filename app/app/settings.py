9806cf5c0ef0ac95693ccb43f4ec350472a1c40c
Aldo Martinez <aldo.martinez@my.utsa.edu>
2023-05-31 16:07:36 -0500

    fixed settings line 82, left randome quotes

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("DB_HOST"),
-        ""
+        "HOST": os.environ.get("DB_HOST")
     }
 }
 
