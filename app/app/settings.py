0793aa8e5dccaafc109304cdb84083fe24b501a9
Aldo Martinez <aldo.martinez@my.utsa.edu>
2023-06-01 13:35:55 -0500

    configured drf spectacular api schema docs.

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9lvaad3ofy&4f*iv%1$55tkswuv7lws@dsz+io21^k$i11*qs@"
+SECRET_KEY = (
+    "django-insecure-9lvaad3ofy&4f*iv%1$55tkswuv7lws@dsz+io21^k$i11*qs@"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    # 3rd party
+    "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +135,7 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
